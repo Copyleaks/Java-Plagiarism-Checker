aab0d9662591836d43597a85ca4e8beb5cdf2f4a
rami-amasha-copyleaks <ramia@copyleaks.com>
2025-09-30 13:44:45 +0300

    Update CopyleaksAiImageDetectionModels.java
    added description for the class

--- src/main/java/models/constants/CopyleaksAiImageDetectionModels.java
+++ src/main/java/models/constants/CopyleaksAiImageDetectionModels.java
@@ -1,5 +1,9 @@
 package models.constants;
 
+/**
+ * Constants for Copyleaks AI Image Detection models.
+ * This class contains the available AI detection model to use for analysis.
+ */
 public class CopyleaksAiImageDetectionModels {
 
     public static final String AI_IMAGE_1_ULTRA = "ai-image-1-ultra";
